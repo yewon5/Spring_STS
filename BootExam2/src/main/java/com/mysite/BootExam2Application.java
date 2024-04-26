305da247d7c415e570ff8110a20ec23f7b503af8
won <ik_324@naver.com>
2024-04-26 18:19:48 +0900

    BootExam2

---
+++ BootExam2/src/main/java/com/mysite/BootExam2Application.java
@@ -0,0 +1,13 @@
+package com.mysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootExam2Application.class, args);
+	}
+
+}
