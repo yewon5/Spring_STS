ff7b90618b88bb97e44bb8b9b15746118242b6ba
won <ik_324@naver.com>
2024-06-26 14:34:45 +0900

    JpaExam3

--- JpaExam3/src/main/java/com/mysite/controllor/JpaController.java
+++ JpaExam3/src/main/java/com/mysite/controllor/JpaController.java
@@ -2,7 +2,10 @@ package com.mysite.controllor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,6 +53,8 @@ public class JpaController {
 		return "redirect:/"; //등록 후 다시 새로고침 되어야 해서 리다이렉트
 	}
 	
+	/*
+	// 검색 쿼리 만들기
 	@GetMapping("/memberList")
 	public String memberList(
 				Model model, 
@@ -63,30 +68,115 @@ public class JpaController {
 		Page<Member> members = null;
 		if(searchCate.equals("name")) {
 			members = memberRepository.findByNameContaining(searchKeyword, pageable);
+			
+			// 이름과 아이디가 같은 사람을 조회
+			// members = memberRepository.findByNameAndId("홍길동", "hong", pageable);
+			
+			// 이름 또는 아이디에 "홍"이라는 값을 갖는 회원을 조회
+			// members = memberRepository.findByNameContainsOrIdContains("홍", "홍", pageable);
 		}
 		else if(searchCate.equals("id")) {
 			members = memberRepository.findById(searchKeyword, pageable);
 		}
 		else if(searchCate.equals("phone")) {
 			// members = memberRepository.findByPhoneContaining(searchKeyword, pageable);
-			// `members = memberRepository.findByPhone(searchKeyword, pageable);
+			// members = memberRepository.findByPhone(searchKeyword, pageable);
+			members = memberRepository.findByPhoneLike("%"+searchKeyword+"%", pageable);
 		}
 		else {
 			members = memberRepository.findAll(pageable);
 		}
 		
-		/*
-		System.out.println("총 페이지 수 : " + members.getTotalPages());
-		System.out.println("총 레코드 수 : " + members.getTotalElements());
-		System.out.println("현재 페이지 번호 : " + members.getNumber());
-		System.out.println("한 페이지에 보여질 레코드 수 : " + members.getSize());
-		System.out.println("정렬 : " + members.getSort());
-		System.out.println(members.getPageable());
-		*/
+		
+//		System.out.println("총 페이지 수 : " + members.getTotalPages());
+//		System.out.println("총 레코드 수 : " + members.getTotalElements());
+//		System.out.println("현재 페이지 번호 : " + members.getNumber());
+//		System.out.println("한 페이지에 보여질 레코드 수 : " + members.getSize());
+//		System.out.println("정렬 : " + members.getSort());
+//		System.out.println(members.getPageable());
+		
 		
 		model.addAttribute("members", members);  //컨트롤러부터 members라는 이름으로 전달 받음. 모델에 담아서 페이지 넘겨주기
 		return "jpa/memberList";
 	}
+	*/
+	
+	/*
+	// 정렬 쿼리 만들기
+	@GetMapping("/memberList")
+	public String memberList(
+				Model model, 
+				@PageableDefault(size=5, sort="name", direction=Sort.Direction.ASC) Pageable pageable,
+				@RequestParam(value="searchKeyword", required = false, defaultValue = "") String searchKeyword) {
+
+		Page<Member> members = null;
+
+		if (searchKeyword.isEmpty()) {
+			// 전체 검색
+			members = memberRepository.findAll(pageable);
+		}
+		else {
+			members = memberRepository.findByNumGreaterThanEqualOrderByNameAsc(Integer.parseInt(searchKeyword), pageable); 
+		}
+		
+		model.addAttribute("members", members); 
+		model.addAttribute("searchKeyword", searchKeyword);
+		return "jpa/memberList";
+	}
+	*/
+	
+	/*
+	// PageRequest.of()
+	@GetMapping("/memberList")
+	public String memberList(
+				Model model, 
+				Pageable pageable,
+				@RequestParam(value="searchKeyword", required = false, defaultValue = "") String searchKeyword) {
+
+		Page<Member> members = null;
+
+		if (searchKeyword.isEmpty()) {
+			// 전체 검색
+			pageable = PageRequest.of(pageable.getPageNumber(), 5, Sort.Direction.ASC, "num"); //(시작 페이지 번호, 한페이지에 보여질 글) 0으로 고정하면 페이지 이동이 안됨. pageable.getPageNumber()해야 페이지 이동됨
+			members = memberRepository.findAll(pageable); 
+		}
+		else {
+			pageable = PageRequest.of(pageable.getPageNumber(), 5, Sort.by("age").descending().and(Sort.by("name").descending())); // 나이/이름 내림차순으로 다중정렬이 가능함
+			members = memberRepository.findByNameContaining(searchKeyword, pageable); // 정렬은 안되고 이름만 검색
+		}
+		
+		model.addAttribute("members", members); 
+		model.addAttribute("searchKeyword", searchKeyword);
+		return "jpa/memberList";
+	}
+	*/
+	
+	
+	// @Query
+	@GetMapping("/memberList")
+	public String memberList(
+				Model model, 
+				@PageableDefault(size=5) Pageable pageable,
+				@RequestParam(value="searchKeyword", required = false, defaultValue = "") String searchKeyword) {
+
+		Page<Member> members = null;
+
+		if (searchKeyword.isEmpty()) {
+			// 전체 검색
+			members = memberRepository.findAll(pageable); 
+		}
+		else { // 이름으로 전달 받으면
+			PageRequest.of(pageable.getPageNumber(), 2);
+			//members = memberRepository.findByName(searchKeyword, pageable); 
+			//members = memberRepository.findByName(searchKeyword, 20, pageable);
+			members = memberRepository.findByNameContainsAndAgeBetweenOrderByNameAsc(searchKeyword, 20, 30, pageable);
+		}
+		
+		model.addAttribute("members", members); 
+		model.addAttribute("searchKeyword", searchKeyword);
+		return "jpa/memberList";
+	}
+	
 	
 	@GetMapping("/memberDelete")
 	public String memberDelete(@RequestParam(value="num", required = false) Integer num) {
--- JpaExam3/src/main/java/com/mysite/repository/MemberRepository.java
+++ JpaExam3/src/main/java/com/mysite/repository/MemberRepository.java
@@ -3,10 +3,16 @@ package com.mysite.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mysite.entity.member.Member;
 
+import jakarta.transaction.Transactional;
+
 public interface MemberRepository extends JpaRepository<Member, Integer>{
+	/******************************** 검색을 위한 쿼리 메서드 ********************************/
 	// 회원 이름으로 검색
 	Page<Member> findByNameContaining(String name, Pageable pageable);
 	
@@ -15,5 +21,44 @@ public interface MemberRepository extends JpaRepository<Member, Integer>{
 	
 	// 회원 전화번호 검색
 	// Page<Member> findByPhoneContaining(String id, Pageable pageable);
-	Page<Member> findByPhone(String id, Pageable pageable);
+	// Page<Member> findByPhone(String id, Pageable pageable);
+	
+	// LIKE - 특정 문자열이 포함되었는지 찾으려면? => %111%
+	Page<Member> findByPhoneLike(String id, Pageable pageable);
+	
+	// And쿼리 : 이름과 아이디가 같은 회원을 조회
+	Page<Member> findByNameAndId(String name, String id, Pageable pageable); //매개변수도 이름 아이디 순서로 받아야 함
+	
+	// Or쿼리 : 이름 또는 아이디에 "홍"이라는 값을 갖는 회원을 조회
+	Page<Member> findByNameContainsOrIdContains(String name, String id, Pageable pageable); //'홍' 포함이라서 Contains
+	
+	
+	/******************************** 정렬을 위한 쿼리 메서드 ********************************/
+	// 회원 등록 순으로 정렬 => 조건, 정렬
+	// 조건 : 회원 번호가 크거나 같은 사람들 중에서 이름 순으로 정렬 GreaterThanEqual >=, LessThanEqual <=
+	Page<Member> findByNumGreaterThanEqualOrderByNameAsc(Integer num, Pageable pageable); //페이징을 안할 경우 List<Member>로 받는다.
+	
+	
+	/************************************** @Query **************************************/
+	// 회원 이름으로 검색
+//	@Query("SELECT m FROM Member m WHERE m.name = ?1 and m.age>20 ORDER BY m.age ASC") //위에서는 메서드 이름이 쿼리를 책임졌지만 여기서는 어노테이션이 쿼리를 책임진다. ?1은 파라미터의 순서를 뜻함
+//	public Page<Member> findByName(String name, Pageable pageable);//메서드에 영향을 받지 않음 
+	
+	// 이름에 "길동"이 들어가고 나이가 20살 이상이고 정렬은 이름 기준으로 오름차순 정렬하시오.
+//	@Query("SELECT m FROM Member m WHERE m.name LIKE %?1% and m.age>=?2 ORDER BY m.name ASC")
+//	public Page<Member> findByName(String name, int age, Pageable pageable); // 나이 값이 정해진 것이 아니라면 파라미터로 전달 받야야 한다.
+	
+	// 이름에 "길동"이 들어가고 나이가 20~30살이고 정렬은 이름 기준으로 오름차순 정렬하시오.
+	@Query("SELECT m FROM Member m WHERE (m.name LIKE %?1%) and (m.age BETWEEN 20 AND 30) ORDER BY m.name ASC")
+	public Page<Member> findByName(String name, int age, Pageable pageable);
+	
+	// 위의 쿼리를 메서드로 조합해서 만드시오.
+	Page<Member> findByNameContainsAndAgeBetweenOrderByNameAsc(String name, int start, int end, Pageable pageable);
+	
+	// 기본키를 기준으로 이름과 나이를 수정하는 JPQL을 작성하시오.
+	// 수정할때는 2가지 어노테이션이 반드시 있어야 함.
+	@Transactional
+	@Modifying 
+	@Query("UPDATE Member m SET m.name=:name, m.age=:age WHERE m.num=:num") //테이블 이름 대소문자 구분
+	int updateMemberQuery(@Param("name") String name, @Param("age") int age);
 }
